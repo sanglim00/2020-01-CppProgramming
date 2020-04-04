f327f6bbbc622d9cd180ee6955a8670fc1fc8cb7
Sanglim00 <54923245+Sanglim00@users.noreply.github.com>
2020-04-04 15:57:37 +0900

    Update Hourglass.cpp

--- Hourglass.cpp
+++ Hourglass.cpp
@@ -15,20 +15,25 @@ int main() {
 
 		cin >> num;
 
+		int n=0;
 		for (int j = num; j >= 1; j--) {
 			cout << endl;
 			for (int k = 0; k < num; k++) {
-				if (j <=num / 2 + 1&& k < j - 1) {
+				if (j <=num / 2 + 1&& k < j - 1) { 
 					cout << "-";
 				}
+
 				else if (j > num / 2  && j <= k ) {
 					cout << "-";
 				}
-				else if (j < num- k && j/2  <= num/2 && k < num/2 && j>k+1) {
-					cout << "-";
-			
-				}
 
+				else if (j < num - k && j / 2 <= num / 2 && k < num / 2 && j>k + 1) {
+					cout << "-";
+				}
+				else if (j <= num / 2 && k >num/2 && k +n>j/2 ) {
+					cout << "-";
+					n--;
+				}
 				
 				else {
 					if (k % 2 == 1)
