0a9737dd0812bb429e1d8c184e1345e6b1e8153b
Sanglim00 <54923245+Sanglim00@users.noreply.github.com>
2020-04-09 21:18:26 +0900

    Update TypeOfTriangle02.cpp

--- TypeOfTriangle02.cpp
+++ TypeOfTriangle02.cpp
@@ -4,7 +4,7 @@ Problem : 삼각형의 종류 02
 ****************************/
 
 #include <iostream>
-#include <cmath>
+#include <math.h>
 
 using namespace std;
 
@@ -24,16 +24,20 @@ int main() {
 		a = sqrt((Carray[1][0] - Carray[0][0]) * (Carray[1][0] - Carray[0][0]) + (Carray[1][1] - Carray[0][1]) * (Carray[1][1] - Carray[0][1]));
 		b = sqrt((Carray[2][0] - Carray[0][0]) * (Carray[2][0] - Carray[0][0]) + (Carray[2][1] - Carray[0][1]) * (Carray[2][1] - Carray[0][1]));
 		c = sqrt((Carray[2][0] - Carray[1][0]) * (Carray[2][0] - Carray[1][0]) + (Carray[2][1] - Carray[1][1]) * (Carray[2][1] - Carray[1][1]));
-		
-		bool Slope = ((Carray[1][1] - Carray[0][1]) / (Carray[1][0] - Carray[0][0])) == ((Carray[2][1] - Carray[1][1]) / (Carray[2][0] - Carray[1][0]));
-		
-		if (a*a + b*b == c*c || a*a + c*c == b*b || b*b + c*c == a*a)
-			cout << 1 << endl;
-		else if (Slope)
+
+		if (a + b > c) {
+			if (a * a + b * b == c * c || a * a + c * c == b * b || b * b + c * c == a * a)
+				cout << 1 << endl;
+			else if (((Carray[1][1] - Carray[0][1]) / (Carray[1][0] - Carray[0][0])) == ((Carray[2][1] - Carray[1][1]) / (Carray[2][0] - Carray[1][0])))
+				cout << 0 << endl;
+			else if (a * a + b * b < c * c || a * a + c * c < b * b || b * b + c * c < a * a)
+				cout << 2 << endl;
+			else if (a * a + b * b > c* c || a * a + c * c > b* b || b * b + c * c > a* a)
+				cout << 3 << endl;
+		}
+		else
 			cout << 0 << endl;
-		else if (a*a + b*b < c*c || a*a + c*c < b*b || b*b +c* c < a*a)
-			cout << 2 << endl;
-		else if (a*a + b*b > c*c || a*a + c*c > b*b || b*b + c*c > a*a)
-			cout << 3 << endl;
+
+		
 	}
 }
