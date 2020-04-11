3171ec12d05107eb07ca6fe0a885db5952abf31e
Sanglim00 <54923245+Sanglim00@users.noreply.github.com>
2020-04-11 16:48:00 +0900

    Update VerticalHorizontal.cpp

--- VerticalHorizontal.cpp
+++ VerticalHorizontal.cpp
@@ -4,6 +4,7 @@ Problem : 수직 수평 선분의 교차
 ****************************/
 
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -11,7 +12,7 @@ int main() {
 
 	int times;
 	int x1, x2, x3, x4, y1, y2, y3, y4;
-
+	int minX, maxX, minY, maxY;
 
 	cin  >> times;
 	for (int i = 0; i < times; i++) {
@@ -19,28 +20,63 @@ int main() {
 		cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4;
 
 		if (x1 == x2) {
-			if (x3 < x1 && x1 < x4) {
-				if (y2 < y3 && y3 < y1) {
+			minX = min(x3, x4);
+			maxX = max(x3, x4);
+ 			minY = min(y1, y2);
+			maxY = max(y1, y2);
+			if (minX < x1 && x1 < maxX) {
+				if (minY < y3 && y3 < maxY) {
 					cout << 1 << endl;
 				}
-				else if (y1 == y3 ||y2 ==y3) {
+				else if (minY == y3 || maxY == y3) {
 					cout << 2 << endl;
 				}
-				else if(y3>y1 || y3<y2) {
+				else {
 					cout << 0 << endl;
 				}
 			}
-			else if (x3 == x1 || x1 == x4 ) {
-				if (y2 <= y3 && y3 <= y1) {
+			else if (minX == x1 || x1 == maxX) {
+				if (minY <= y3 && y3 <= maxY) {
 					cout << 2 << endl;
 				}
-				else if(y3<y2 ||y3>y1) {
+				else {
 					cout << 0 << endl;
 				}
 			}
 			else {
 				cout << 0 << endl;
 			}
+		}
+		else if (x3 == x4) {
+			minX = min(x1, x2);
+			maxX = max(x1, x2);
+			minY = min(y3, y4);
+			maxY = max(y3, y4);
+			if (minX < x3 && x3 < maxX) {
+				if (minY < y1 && y1 < maxY) {
+					cout << 1 << endl;
+				}
+				else if (minY == y1 || maxY == y1) {
+					cout << 2 << endl;
+				}
+				else {
+					cout << 0 << endl;
+				}
+			}
+			else if (minX == x3 || maxX == x3) {
+				if ( minY <= y1 && y1 <= maxY) {
+					cout << 2 << endl;
+				}
+				else {
+					cout << 0 << endl;
+				}
+			}
+			else {
+				cout << 0 << endl;
+			}
+		}
+		else {
+			cout << 0 << endl;
 		}
 	}
 }
