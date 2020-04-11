917eddefb98608ec685b4583d0d1693fd06a6501
Sanglim00 <54923245+Sanglim00@users.noreply.github.com>
2020-04-11 15:40:31 +0900

    Update VerticalHorizontal.cpp

--- VerticalHorizontal.cpp
+++ VerticalHorizontal.cpp
@@ -12,32 +12,35 @@ int main() {
 	int times;
 	int x1, x2, x3, x4, y1, y2, y3, y4;
 
+
 	cin  >> times;
 	for (int i = 0; i < times; i++) {
 
 		cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4;
 
-		if ((x3 < x1 && x1 < x4 )|| (x3 < x2 && x2 < x4)) {
-			if ((y2 < y3 && y3 < y1) || (y2 < y4 && y4 < y1)) {
-				cout << 1 << endl;
+		if (x1 == x2) {
+			if (x3 < x1 && x1 < x4) {
+				if (y2 < y3 && y3 < y1) {
+					cout << 1 << endl;
+				}
+				else if (y1 == y3 ||y2 ==y3) {
+					cout << 2 << endl;
+				}
+				else if(y3>y1 || y3<y2) {
+					cout << 0 << endl;
+				}
 			}
-			else if ((y1 >= y3 && y2 <=y3) || (y1 >= y4 || y2 <= y4)) {
-				cout << 2 << endl;
+			else if (x3 == x1 || x1 == x4 ) {
+				if (y2 <= y3 && y3 <= y1) {
+					cout << 2 << endl;
+				}
+				else if(y3<y2 ||y3>y1) {
+					cout << 0 << endl;
+				}
 			}
 			else {
 				cout << 0 << endl;
 			}
-		}
-		else if (x3 == x1 || x1 == x4 || x3 == x2 || x2 == x4) {
-			if ((y2 <= y3 && y3 <= y1) || (y2 <= y4 && y4 <= y1)) {
-				cout << 2 << endl;
-			}
-			else {
-				cout << 0 << endl;
-			}
-		}
-		else {
-			cout << 0 << endl;
 		}
 	}
 }
