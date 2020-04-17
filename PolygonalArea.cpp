1edc5a338bacff0820d62e618e57923c7af6d479
Sanglim00 <54923245+Sanglim00@users.noreply.github.com>
2020-04-17 22:00:08 +0900

    Add files via upload

---
+++ PolygonalArea.cpp
@@ -0,0 +1,33 @@
+/* **************************
+Problem : 다각형의 면적
+20191584 남상림
+****************************/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	int times, num;
+	int x, y, D;
+	int area = 0;
+	int arrX[50] = {};
+	int arrY[50] = {};
+
+	cin >> times;
+	for (int i = 0; i < times; i++) {
+		cin >> num;
+		for (int j = 0; j < num; j++) {
+			cin >> x >> y;
+			arrX[j] = x;
+			arrY[j] = y;
+		}
+		for (int a = 0; a < num-1 ; a++) {
+			area += (arrX[a] + arrX[a + 1]) * (arrY[a + 1] - arrY[a]);	
+		}
+		area += (arrX[0] + arrX[num-1]) * (arrY[0] - arrY[num-1]);	
+		D = (area > 0) ? 1 : -1;
+		cout << abs(area) <<" " << D << endl;
+	}
+}
