a284aee1f2bbd62aef5c44789ddd009a8779fabc
Sanglim00 <54923245+Sanglim00@users.noreply.github.com>
2020-04-29 15:12:42 +0900

    Add files via upload

---
+++ MatrixMultiplication.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int times, num;
+	int r, s, t;
+
+	cin >> times;
+	for (int i = 0; i < times; i++) {
+		cin >> r >> s >> t;
+
+		int** arr = new int* [r];
+		for (int j = 0; j < r; j++) arr[j] = new int[s];
+
+		for (int j = 0; j < r; j++) {
+			for (int k = 0; k < s; k++) {
+				cin >> num;
+				arr[j][k] = num;
+			}
+		}
+
+		for (int j = 0; j < r; j++) {
+			for (int k = 0; k < s; k++) {
+				cout << arr[j][k] << " ";
+			}
+			cout << endl;
+		}
+
+		int** arr2 = new int* [s];
+		for (int j = 0; j < s; j++) arr2[j] = new int[t];
+
+		for (int j = 0; j < s; j++) {
+			for (int k = 0; k < t; k++) {
+				cin >> num;
+				arr2[j][k] = num;
+			}
+		}
+
+		for (int j = 0; j < s; j++) {
+			for (int k = 0; k < t; k++) {
+				cout << arr2[j][k] << " ";
+			}
+			cout << endl;
+		}
+
+		int** multi = new int* [r];
+		for (int j = 0; j < r; j++) multi[j] = new int[t];
+
+		for (int a = 0; a < r; r++) {
+			for (int b = 0; b < t; b++) {
+				multi[a][b] = 0;
+				for (int c = 0; c < s; c++) {
+					multi[a][b] += arr[a][c] * arr2[c][b];
+				}
+			}
+		}
+		for (int j = 0; j < r; j++) {
+			for (int k = 0; k < t; k++) {
+				cout << multi[j][k] << " ";
+			}
+			cout << endl;
+		}
+	}
+}
