ff539a604f2d014dee7af6fa40ff97cace38c66b
Sanglim00 <54923245+Sanglim00@users.noreply.github.com>
2020-05-01 05:30:01 +0900

    Add files via upload

---
+++ GetRoomSize.cpp
@@ -0,0 +1,31 @@
+/* **************************
+Problem : 방의 크기 구하기
+20191584 남상림
+****************************/
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+	int times;
+	int m, n;
+	string line;
+
+	cin >> times;
+	for (int i = 0; i < times; i++) {
+		cin >> m >> n;
+
+		char** room = new char* [n];
+		for (int j = 0; j < n; j++) room[j] = new char[m];
+
+		for (int j = 0; j < n; j++) {
+			cin >> line;
+			for (int k = 0; k < m; k++) {
+				room[j][k] = line[k];
+			}
+		}
+		
+	}
+}
