63f1821982a08f1aeb82240cc235f3efe58b4a90
Sanglim00 <54923245+Sanglim00@users.noreply.github.com>
2020-05-01 04:16:09 +0900

    Update Parentheses.cpp

--- Parentheses.cpp
+++ Parentheses.cpp
@@ -2,38 +2,53 @@
 Problem : 괄호
 20191584 남상림
 ****************************/
-#include <iostream>
-#include <string>
-#include <stack>
+#include<iostream>
+#include<stack>
+#include<string>
 
 using namespace std;
 
+bool match(char a, char b) {
+    switch (a) {
+    case '(':
+        if (b == ')') return true;
+        else return false;
+        break;
+    case '{':
+        if (b == '}') return true;
+        else return false;
+        break;
+    case '[':
+        if (b == ']') return true;
+        else return false;
+        break;
+    default:
+        return false;
+    }
+}
 int main() {
-	int times, answer;
-	string str;
-	stack<char> STK;
-	char temp;
+    int times; 
 
-	cin >> times;
-	for (int i = 0; i < times; i++) {
-		cin >> str;
-		char* buffer = new char[str.length()+1];
-		str.copy(buffer, str.length() + 1);
-		for (int j = 0; j < str.length(); j++) {
-			if (buffer[j] == '(' || buffer[j] == '{' || buffer[j] == '[') STK.push(buffer[i]);
-			if (buffer[j] == ')' || buffer[j] == '}' || buffer[j] == ']') {
-				if (STK.empty()) answer = 0;
-				else {
-					temp = STK.top();
-					STK.pop();
-					if (temp!= buffer[j]) answer = 0;
-					else answer = 1;
-				}
-			}
-		}
-		if (STK.empty()) answer = 1;
-		else answer = 0;
-		cout << answer << endl;
-		
-	}
+    cin >> times;
+    for (int i = 0; i < times; i++) {
+       
+        int answer = 0;
+        string str;
+        stack<char> STK;
+
+        cin >> str;
+        for (int j = 0; j < str.length(); j++) {
+            if (str[j] == '(' || str[j] == '[' || str[j] == '{') STK.push(str[j]);
+            else {
+                if (STK.empty()) { answer = 0; break; }
+                else {
+                    if (match(STK.top(), str[j])) { STK.pop(); answer = 1; }
+                    else if (!match(STK.top(), str[j])) { STK.pop(); answer = 0; break; }
+                }
+            }
+        }
+        if (!STK.empty()) answer = 0;
+        cout << answer << endl;
+
+    }
 }
