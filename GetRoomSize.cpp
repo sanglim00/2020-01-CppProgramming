7a4e703a75a4161253b2c4fda6f8318fdc46bc0e
Sanglim00 <54923245+Sanglim00@users.noreply.github.com>
2020-05-02 04:37:25 +0900

    Update GetRoomSize.cpp

--- GetRoomSize.cpp
+++ GetRoomSize.cpp
@@ -10,29 +10,28 @@ Problem : 방의 크기 구하기
 using namespace std;
 
 struct element { char y, x; };
-element moveD[4]= { {1, 0}, {-1, 0}, {0, 1}, {0, -1} };
- 
+element moveD[4] = { {1, 0}, {-1, 0}, {0, 1}, {0, -1} };
+
 char room[100][100];
-char visited[100][100] = {0,};
+char visited[100][100] ;
 
 int m, n;
 int cnt;
 void HowManyRoom(int y, int x) {
 	visited[y][x] = '+';
-	cnt ++;
+	cnt++;
 
 	for (int i = 0; i < 4; i++) {
 		int next_Y = y + moveD[i].y;
 		int next_X = x + moveD[i].x;
 
 		if (0 < next_Y && next_Y < m && 0 < next_X && next_X < n) {
-			if (room[next_Y][next_X] == '.'&& visited[next_Y][next_X] !='+') HowManyRoom(next_Y,next_X);
+			if (room[next_Y][next_X] == '.' && visited[next_Y][next_X] != '+') HowManyRoom(next_Y, next_X);
 		}
 	}
 }
-int main(){
+int main() {
 	int times;
-	vector<int>Rcount;
 
 	cin >> times;
 	for (int i = 0; i < times; i++) {
@@ -44,27 +43,21 @@ int main(){
 				room[j][k] = line[k];
 			}
 		}
+		vector<int>Rcount;
+
 		for (int j = 0; j < n; j++) {
 			for (int k = 0; k < m; k++) {
-				if (room[j][k] == '.' && visited[j][k]!='+') {
+				if (room[j][k] == '.' && visited[j][k] != '+') {
 					cnt = 0;
 					HowManyRoom(j, k);
 					Rcount.push_back(cnt);
 				}
 			}
 		}
-		for (int j = 0; j < n; j++) {
-			for (int k = 0; k < m; k++) {
-				cout << visited[j][k];
-			}
-			cout<<endl;
-		}
-
 		cout << Rcount.size() << endl;
 		sort(Rcount.begin(), Rcount.end(), greater<int>());
 		for (int c = 0; c < Rcount.size(); c++) cout << Rcount[c] << " ";
 		cout << endl;
-		Rcount.clear();
 	}
 	return 0;
 }
