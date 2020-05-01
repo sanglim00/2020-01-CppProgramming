0f16eb7fc2161fd841c56f19941f45e1a2e263b9
Sanglim00 <54923245+Sanglim00@users.noreply.github.com>
2020-05-02 05:24:10 +0900

    Update GetRoomSize.cpp

--- GetRoomSize.cpp
+++ GetRoomSize.cpp
@@ -12,53 +12,54 @@ using namespace std;
 struct element { char y, x; };
 element moveD[4] = { {1, 0}, {-1, 0}, {0, 1}, {0, -1} };
 
-char room[100][100];
-char visited[100][100];
+int room[100][100];
+int visited[100][100];
+int m, n, cnt;
 
-int m, n;
-int cnt;
 void HowManyRoom(int y, int x) {
-	visited[y][x] = '+';
-	cnt++;
+    visited[y][x] = 1;
+    cnt++;
+    for (int i = 0; i < 4; i++) {
+        int next_Y = y + moveD[i].y;
+        int next_X = x + moveD[i].x;
 
-	for (int i = 0; i < 4; i++) {
-		int next_Y = y + moveD[i].y;
-		int next_X = x + moveD[i].x;
+        if (0 <= next_Y && next_Y < n && 0 <= next_X && next_X < m) {
+            if (room[next_Y][next_X] == 1 && visited[next_Y][next_X] == 0)
+                HowManyRoom(next_Y, next_X);
+        }
+    }
 
-		if (0 <= next_Y && next_Y < n && 0 <= next_X && next_X < m) {
-			if (room[next_Y][next_X] == '.' && visited[next_Y][next_X] != '+') 
-				HowManyRoom(next_Y, next_X);
-		}
-	}
 }
 int main() {
-	int times;
+    int times;
 
-	cin >> times;
-	for (int i = 0; i < times; i++) {
-		string line;
-		cin >> m >> n;
-		for (int j = 0; j < n; j++) {
-			cin >> line;
-			for (int k = 0; k < m; k++) {
-				room[j][k] = line[k];
-			}
-		}
-		vector<int>Rcount;
+    cin >> times;
+    for (int i = 0; i < times; i++) {
+        string line;
+        cin >> m >> n;
+        for (int j = 0; j < n; j++) {
+            cin >> line;
+            for (int k = 0; k < m; k++) {
+                if (line[k] == '+') room[j][k] = 0;
+                else if (line[k] == '.') room[j][k] = 1;
+            }
+        }
 
-		for (int j = 1; j < n; j++) {
-			for (int k = 1; k < m; k++) {
-				if (room[j][k] == '.' && visited[j][k] != '+') {
-					cnt = 0;
-					HowManyRoom(j, k);
-					Rcount.push_back(cnt);
-				}
-			}
-		}
-		cout << Rcount.size() << endl;
-		sort(Rcount.begin(), Rcount.end(), greater<int>());
-		for (int c = 0; c < Rcount.size(); c++) cout << Rcount[c] << " ";
-		cout << endl;
-	}
-	return 0;
+        vector<int>Rcount;
+
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < m; k++) {
+                if (room[j][k] == 1 && visited[j][k] == 0) {
+                    cnt = 0;
+                    HowManyRoom(j, k);
+                    Rcount.push_back(cnt);
+                }
+            }
+        }
+        cout << Rcount.size() << endl;
+        sort(Rcount.begin(), Rcount.end(), greater<int>());
+        for (int c = 0; c < Rcount.size(); c++) cout << Rcount[c] << " ";
+        cout << endl;
+    }
+    return 0;
 }
