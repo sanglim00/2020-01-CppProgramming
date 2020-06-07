939413e49580045a1b311797c24b9ada937c5ff2
NamSanglim <54923245+NamSanglim@users.noreply.github.com>
2020-06-07 19:47:26 +0900

    Update MyPolynomial.h

--- Polynomial/MyPolynomial.h
+++ Polynomial/MyPolynomial.h
@@ -1,45 +1,49 @@
-#ifndef _MYPOLYNOMIAL_H_
-#define _MYPOLYNOMIAL_H_
 #include <iostream>
 #include <list>
+#include <algorithm>
+#include <cmath>
 #include "MyTerm.h"
 
 using namespace std;
 
+#ifndef _MYPOLYNOMIAL_H_
+#define _MYPOLYNOMIAL_H_
+
 class myPolynomial {
 private:
-	int degree; // maximum expnent
-	int numTerm;
-	myTerm* Mterm;
-	 /********************* add your code here ********************************/
-
+    int degree;
+    int numTerm;
+    myTerm Mterm[111];
 public:
-	myPolynomial(int c = 0, unsigned e = 0);
-	myPolynomial(int nTerms, int mono[]);
-	// copy constructor
-	myPolynomial(const myPolynomial& poly);
-	// overloaded operators
-	bool operator == (const myPolynomial& poly) const;
-	bool operator != (const myPolynomial& poly) const;
-	myPolynomial& operator += (const myPolynomial& poly);
-	myPolynomial& operator -= (const myPolynomial& poly);
-	myPolynomial& operator *= (const myPolynomial& poly);
-	myPolynomial& operator *= (int k);
-	myPolynomial operator -() const;
-	myPolynomial operator *(int k) const;
-	myPolynomial operator +(const myPolynomial& poly) const;
-	myPolynomial operator -(const myPolynomial& poly) const;
-	myPolynomial operator *(const myPolynomial& poly) const;
-	long operator() (int x) const; // evaluate the polynomial
-	int getDegree() const; // get a degree of the polynomial
-	unsigned getNumTerms() const; // number of terms in the polynomial
-	myPolynomial ddx() const; // derivative of a polynomial
-	// friend operators and functions
-	friend myPolynomial operator *(int k, const myPolynomial& poly);
-	friend ostream& operator <<(ostream& outStream, const myPolynomial& poly);
-	static const myPolynomial ZERO; // P(x) = 0
-	static const myPolynomial ONE; // P(x) = 1
-	static const myPolynomial X; // P(x) = x
+    myPolynomial(int c = 0, unsigned e = 0);
+    myPolynomial(int nTerms, int mono[]);
+    myPolynomial(const myPolynomial& poly);
 
+    bool operator == (const myPolynomial& poly) const;
+    bool operator != (const myPolynomial& poly) const;
+
+    myPolynomial& operator += (const myPolynomial& poly);
+    myPolynomial& operator -= (const myPolynomial& poly);
+    myPolynomial& operator *= (const myPolynomial& poly);
+    myPolynomial& operator *= (int k);
+
+    myPolynomial operator -() const;
+    myPolynomial operator *(int k) const;
+    myPolynomial operator +(const myPolynomial& poly) const;
+    myPolynomial operator -(const myPolynomial& poly) const;
+    myPolynomial operator *(const myPolynomial& poly) const;
+
+    long operator() (int x) const;
+    int getDegree() const;
+    unsigned getNumTerms() const;
+
+    myPolynomial ddx() const;
+
+    friend myPolynomial operator *(int k, const myPolynomial& poly);
+    friend ostream& operator <<(ostream& outStream, const myPolynomial& poly);
+
+    static const myPolynomial ZERO;
+    static const myPolynomial ONE;
+    static const myPolynomial X;
 };
-#endif _MYPOLYNOMIAL_H_
+#endif
